9d9fd62f86f1bcb7b12db0d23dbebdd009849212
taymersson <taymersson@gmail.com>
2024-07-23 21:34:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post
+            author="Taymersson PHILIPE"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, sit obcaecati neque nihil aspernatur sequi unde repudiandae pariatur officia mollitia cupiditate, suscipit impedit voluptates iure deleniti. Quos laboriosam neque culpa."
+          />
+          <Post
+            author="Prisco CLEYTON"
+            content="EU SOU LINDO"
+          />
+          <Post 
+            author="Nemesio GRANDSON"
+            content="EU SOU BONITO TAMBEM"
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+console.log(styles);
+
+import igniteLogo from '../assets/ignite-logo.svg';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo Ignite"/>
+            <h1>Ignite Feed</h1>
+        </header>
+    );
+}
